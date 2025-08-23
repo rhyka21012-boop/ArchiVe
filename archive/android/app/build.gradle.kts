aa9b2ec84ec901aab20051dc50d7b37ba9f9c2b3
rhyka21012-boop <rhyka21012@gmail.com>
2025-08-23 22:56:56 +0900

    CodeMagicのAndroidビルド不具合対応3
    対応もれを修正

--- archive/android/app/build.gradle.kts
+++ archive/android/app/build.gradle.kts
@@ -37,9 +37,6 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         //minSdk = flutter.minSdkVersion
-
-        // local.properties から flutter SDK の値を読み込む
-        val localProperties = gradleLocalProperties(rootDir)
         
         minSdk = 23
         targetSdk = 34
